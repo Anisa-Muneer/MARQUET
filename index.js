87f824f648090de76bc998d05a42aec9f9cc97f8
Anisa Muneer <anisamuneer7@gmail.com>
2024-03-28 23:12:16 +0530

    env

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
 const dotenv = require('dotenv')
 dotenv.config();
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 mongoose.connect(process.env.mongo)
 
-const express=require("express")
-const app=express()
-const path=require('path')
-const nocache=require('nocache')
+const express = require("express")
+const app = express()
+const path = require('path')
+const nocache = require('nocache')
 
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(nocache());
 
 
 app.set('view engine', 'ejs');
-app.set('views','./views/users')
+app.set('views', './views/users')
 
-const publicpath=path.join(__dirname,'public')
+const publicpath = path.join(__dirname, 'public')
 
 app.use(express.static(publicpath))
 
-const userRouter=require('./routes/userRoute')
-app.use('/',userRouter)
+const userRouter = require('./routes/userRoute')
+app.use('/', userRouter)
 
-const adminRoute=require('./routes/adminRoute');
+const adminRoute = require('./routes/adminRoute');
 const { error } = require('console');
-app.use('/admin',adminRoute)
+app.use('/admin', adminRoute)
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-  });
-  
+  console.error(err);
+  res.status(500).send("Internal Server Error");
+});
 
-
-
-app.listen(2040,()=>{
-    console.log("Server running @ 2040");
-
+const port = process.env.port || 3000
+const server = app.listen(port, () => {
+  console.log(`server is running ${port}`);
 })
